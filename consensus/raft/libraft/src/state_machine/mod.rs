6b64feab69ec1c2a2a6cef89935ffc0cc4daaac9
rink1969 <rink1969@gmail.com>
2017-07-27 16:08:44 +0800

    add copyright and license

--- consensus/raft/libraft/src/state_machine/mod.rs
+++ consensus/raft/libraft/src/state_machine/mod.rs
@@ -1,3 +1,20 @@
+// CITA
+// Copyright 2016-2017 Cryptape Technologies LLC.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 //! A `StateMachine` is a single instance of a distributed application. It is the `raft` libraries
 //! responsibility to take commands from the `Client` and apply them to each `StateMachine`
 //! instance in a globally consistent order.
