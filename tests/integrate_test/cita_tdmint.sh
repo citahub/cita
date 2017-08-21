3c79bec4ae6591aacef111a686338b8945d368ed
urugang <urugang@cryptape.com>
2017-08-21 17:34:30 +0800

    support docker build

--- tests/integrate_test/cita_tdmint.sh
+++ tests/integrate_test/cita_tdmint.sh
@@ -13,7 +13,7 @@ cd ${CUR_PATH}/../../admintool/
 setup_node() {
     id=$1
     cd ${CUR_PATH}/../../admintool/release/node${id}
-    ./cita setup ${id}
+    sudo ./cita setup ${id}
 }
 
 start_node() {
