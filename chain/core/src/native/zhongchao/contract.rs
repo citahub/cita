11926599273fcb9c587054bfcae10dd2c82c9db6
kaikaifeng <kaikai@cryptape.com>
2017-08-23 15:26:11 +0800

    add the interface into_group adn quit_group

--- chain/core/src/native/zhongchao/contract.rs
+++ chain/core/src/native/zhongchao/contract.rs
@@ -6,13 +6,14 @@
 
 use super::*;
 use native::permission::action::ElementAction;
+use std::str;
 
 pub struct ZcPermission {
     functions: HashMap<Signature, Box<Function>>,
     creator: Vec<String>,
     sender: Vec<String>,
     applicant_of_creator: Vec<String>,
-    applicant_of_senter: Vec<String>,
+    applicant_of_sender: Vec<String>,
 }
 
 impl Contract for ZcPermission {
@@ -28,7 +29,7 @@ impl ZcPermission {
             creator: vec![],
             sender: vec![],
             applicant_of_creator: vec![],
-            applicant_of_senter: vec![],
+            applicant_of_sender: vec![],
         };
         contract.functions.insert(0, Box::new(ZcPermission::apply_group));
         contract.functions.insert(1, Box::new(ZcPermission::verify_group));
@@ -44,26 +45,47 @@ impl ZcPermission {
         Ok(GasLeft::Known(U256::from(0)))
     }
     // verify the application
+    // fn verify_group(group: &string, action: &ElementAction);
     pub fn verify_group(params: &ActionParams, ext: &mut Ext) -> evm::Result<GasLeft<'static>> {
         if let Some(ref data) = params.data {
-            if let Some(data) = data.get(32..68) {
-                let group = data.get(4..36);
+            if let Some(data) = data.get(36..68) {
                 match data[68] {
-                    0 => into_group(&group.unwrap()),
-                    1 => quit_group(&group.unwrap()),
-                    _ => return Ok(GasLeft::Known(U256::from(0))),
-                }
+                    0 => into_group(data.get(4..36).unwrap(), ext),
+                    1 => quit_group(data.get(4..36).unwrap(), ext),
+                    _ => false,
+                };
                 // let _ = ext.set_storage(H256::from(0), H256::from(data));
             }
         }       
         // verify the into application
-        fn into_group(group: &[u8]) {
-                  
+        fn into_group(group: &[u8], ext: &mut Ext) -> bool {
+            match str::from_utf8(group).unwrap() {
+                "sender" => {
+                    // ext.set_storage(H256::from(0), H256::from(data));
+                    true
+                }
+                "creator" => {
+                    // ext.set_storage(H256::from(0), H256::from(data));
+                    true
+                },
+                _ => false,
+            }
         }
         // verify the quit application
-        fn quit_group(group: &[u8]) {
-          
-        }       
+        fn quit_group(group: &[u8], ext: &mut Ext) -> bool {
+            match str::from_utf8(group).unwrap() {
+                "sender" => {
+                    // ext.set_storage(H256::from(0), H256::from(data));
+                    true
+                },
+                "creator" => {
+                    // ext.set_storage(H256::from(0), H256::from(data));
+                    true
+                },
+                _ => false,
+            }
+        }
+
         Ok(GasLeft::Known(U256::from(0)))
     }
 
@@ -73,4 +95,3 @@ impl ZcPermission {
         Ok(GasLeft::Known(U256::from(0)))
     }
 }
-
