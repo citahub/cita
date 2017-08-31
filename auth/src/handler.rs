ad0e754998cbc8511c9feffa9116c879ca7f3e47
rink1969 <rink1969@cryptape.com>
2017-08-31 20:47:06 +0800

    process verify

--- auth/src/handler.rs
+++ auth/src/handler.rs
@@ -17,10 +17,14 @@
 
 use libproto::*;
 use libproto::communication::*;
+use libproto::auth::*;
 use protobuf::core::parse_from_bytes;
+use std::sync::mpsc::Sender;
+use verify::Verifyer;
+use protobuf::{Message, RepeatedField};
+use util::H256;
 
-
-pub fn handle_msg(payload: Vec<u8>) {
+pub fn handle_msg(payload: Vec<u8>, tx_pub: Sender<(String, Vec<u8>)>, v: Verifyer) {
 
     if let Ok(msg) = parse_from_bytes::<communication::Message>(payload.as_ref()) {
         let t = msg.get_field_type();
@@ -30,7 +34,29 @@ pub fn handle_msg(payload: Vec<u8>) {
             match content {
                 MsgClass::STATUS(status) => {
                     let height = status.get_height();
-                    info!("got height {:?}", height);
+                    trace!("got height {:?}", height);
+                    v.set_height(height);
+                }
+                MsgClass::VERIFYREQ(req) => {
+                    trace!("get verify request {:?}", req);
+                    let resps = Vec::new();
+                    for req in req.get_reqs() {
+                        let ret = v.check_hash(&H256::from_slice(req.get_tx_hash()));
+                        if !ret {
+                            let resp = VerifyRespMsg::new();
+                            resp.set_ret(Ret::Dup);
+                            resp.set_tx_hash(req.get_tx_hash().to_vec());
+                            resps.push(resp);
+                        } else {
+                            let resp = VerifyRespMsg::new();
+                            resp.set_ret(Ret::Ok);
+                            resp.set_tx_hash(req.get_tx_hash().to_vec());
+                            resps.push(resp);
+                        }                        
+                    }
+                    let vresq = VerifyResp::new();
+                    vresq.set_resps(RepeatedField::from_slice(&resps));
+                    tx_pub.send(("auth.verify_resp".to_string(), vresq.write_to_bytes().unwrap())).unwrap();
                 }
                 _ => {}
             }
