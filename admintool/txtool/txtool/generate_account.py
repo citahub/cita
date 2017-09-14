4f2db4b93aca36633073aa02e819df6bc305f4e2
Sun Lei <gitter0759@gmail.com>
2017-09-14 16:16:32 +0800

    txtool sender from privkey.

--- admintool/txtool/txtool/generate_account.py
+++ admintool/txtool/txtool/generate_account.py
@@ -6,7 +6,7 @@ import sys
 import binascii
 import argparse
 from pathlib import Path
-from util import which, run_command
+from util import which, run_command, hex2bytes
 from ecdsa import SigningKey, SECP256k1
 import sha3
 import pysodium
@@ -41,15 +41,21 @@ def save_address(address):
     with open("../output/accounts/address", "w+") as address_file:
         address_file.write(address)
 
-def generate(newcrypto=False):
-    if newcrypto:
-        _generate_new()
-    else:
-        _generate_old()
 
-def _generate_old():
+def generate(privkey=None, newcrypto=False):
+    if newcrypto:
+        _generate_new(privkey)
+    else:
+        _generate_old(privkey)
+
+
+def _generate_old(privkey=None):
     keccak = sha3.keccak_256()
-    priv = SigningKey.generate(curve=SECP256k1)
+    if privkey is None:
+        priv = SigningKey.generate(curve=SECP256k1)
+    else:
+        priv = SigningKey.from_string(hex2bytes(privkey), curve=SECP256k1)
+    
     pub = priv.get_verifying_key().to_string()
 
     keccak.update(pub)
@@ -59,7 +65,7 @@ def _generate_old():
     save_pubkey(binascii.hexlify(pub))
     save_address(address)
 
-def _generate_new():
+def _generate_new(privkey=None):
     pk, sk = pysodium.crypto_sign_keypair()
     save_privkey(binascii.b2a_hex(sk))
 
@@ -74,7 +80,7 @@ def main():
     parser.add_argument('--no-newcrypto', dest='newcrypto', action='store_false')
     parser.set_defaults(newcrypto=False)
     args = parser.parse_args()
-    generate(args.newcrypto)
+    generate(None, args.newcrypto)
 
 if __name__ == "__main__":
     main()
