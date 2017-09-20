9b6bec264c9ca044d5d227ef72379376791b6831
suyanlong <592938410@qq.com>
2017-09-20 10:19:45 +0800

    amend fix typo

--- consensus/tdmint/src/core/dispatchtx.rs
+++ consensus/tdmint/src/core/dispatchtx.rs
@@ -131,7 +131,7 @@ impl Dispatchtx {
                     if error_msg.is_some() {
                         response.set_code(submodules::CONSENSUS as i64);
                         tx_response.status = error_msg.unwrap();
-                        response.set_error_msg(format!("{:?}",tx_response));
+                        response.set_error_msg(format!("{:?}", tx_response));
                     } else {
                         let tx_state = serde_json::to_string(&tx_response).unwrap();
                         response.set_tx_state(tx_state);
@@ -147,8 +147,7 @@ impl Dispatchtx {
                     let mut response = Response::new();
                     response.set_request_id(request_id);
                     response.set_code(submodules::AUTH as i64);
-                    let error_msg = serde_json::to_string(&tx_response).unwrap();
-                    response.set_error_msg(error_msg);
+                    response.set_error_msg(format!("{:?}", tx_response));
 
                     let msg = factory::create_msg(submodules::CONSENSUS, topics::RESPONSE, communication::MsgType::RESPONSE, response.write_to_bytes().unwrap());
                     trace!("response new tx {:?}", response);
