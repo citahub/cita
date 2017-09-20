2ecd5f5b48e9277a320e5b7ea9883fd68d637966
rink1969 <rink1969@cryptape.com>
2017-09-20 15:06:05 +0800

    add notice after create protobuf files

--- share_libs/proto/src/create_protobuf.sh
+++ share_libs/proto/src/create_protobuf.sh
@@ -16,3 +16,13 @@ case "$OSTYPE" in
         ;;
 esac
 
+for i in `find . -name "*.rs"`
+do
+    if grep -q -e "Copyright 2015-2017 Parity Technologies" -e "Copyright 2016-2017 Cryptape Technologies" $i
+    then
+        echo "Ignoring the " $i
+    else
+        echo "Starting modify" $i
+        (cat ../../../publishtool/notices | cat - $i > file1) && mv file1 $i
+    fi
+done
