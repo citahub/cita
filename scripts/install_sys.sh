9756d889960b1b9bfa43f5ffb07715b50a53b335
u2 <zhangyaning1985@gmail.com>
2017-10-04 09:58:48 +0800

    Fixed install libsodium

--- scripts/install_sys.sh
+++ scripts/install_sys.sh
@@ -16,10 +16,10 @@ add-apt-repository -y ppa:ethereum/ethereum
 # 4) install dependencies
 apt-get update -q
 # 4.1) deploy dependencies: synch with scripts/Dockerfile-run if modified.
-apt-get install -y libstdc++6 libsodium18 rabbitmq-server libssl-dev libgoogle-perftools4 python-pip
+apt-get install -y libstdc++6 rabbitmq-server libssl-dev libgoogle-perftools4 python-pip
 # 4.2) development dependencies
 apt-get install -y build-essential pkg-config                              \
-        libsnappy-dev  libgoogle-perftools-dev   libsodium-dev libzmq3-dev \
+        libsnappy-dev  libgoogle-perftools-dev   libsodium* libzmq3-dev \
 		solc curl jq  google-perftools capnproto
 wget https://github.com/cryptape/GmSSL/releases/download/v1.0/libgmssl.so.1.0.0.gz
 gzip -d libgmssl.so.1.0.0.gz
