b4ce8e2ec6463d8c83fec68c7614d1121f4428aa
kaikaifeng <kaikai@cryptape.com>
2017-10-13 14:55:18 +0800

    test permission node_manager quota futures and repeat tx

---
+++ scripts/contracts/test_quota_sol.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+    uint storedData;
+
+    event Set(uint);
+
+    function set(uint x) {
+        uint num = 100000;
+        for (uint i = 0; i < num; i++) {
+            storedData = i;
+        }
+
+        Set(x);
+    }
+
+    function get() constant returns (uint) {
+        return storedData;
+    }
+}
