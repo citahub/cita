5bd12ff9078d9aa7369f54f1e1e4b8bcf6a378e1
Zhiwei <rink1969@cryptape.com>
2017-10-21 07:59:06 +0000

    Merged in 377_auto_close_channel (pull request #332)
    clean queues when cita start and bind rabbitmq to localhost
    
    Approved-by: zhangyaning <u2@cryptape.com>
    Approved-by: urugang <urugang@cryptape.com>

--- scripts/config_rabbitmq.sh
+++ scripts/config_rabbitmq.sh
@@ -8,6 +8,13 @@ sudo(){
     fi
     set +o noglob
 }
+
+sudo cat <<EOF > /etc/rabbitmq/rabbitmq.config
+[
+    {rabbit, [{tcp_listeners, [{"127.0.0.1", 5672}]}]}
+].
+EOF
+
 sudo /etc/init.d/rabbitmq-server restart
 (sudo rabbitmqctl list_vhosts | grep dev) && sudo rabbitmqctl delete_vhost dev
 sudo rabbitmqctl add_vhost dev
