56a92250bbb916aa583cf5a1455ea24ba8c6d7a3
suyanlong <592938410@qq.com>
2017-11-22 14:48:52 +0800

    remove WaitTimer::set_timer function
    Signed-off-by: suyanlong <592938410@qq.com>

--- consensus/tdmint/src/main.rs
+++ consensus/tdmint/src/main.rs
@@ -14,7 +14,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#![allow(unused_must_use)]
 #![feature(mpsc_select)]
 #[macro_use]
 extern crate serde_derive;
