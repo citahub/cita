816cbc951ff058b49aa7768921590275e9f4be68
huoqiutianyu <yubo@cryptape.com>
2018-05-22 18:50:43 +0800

    fmt

--- cita-auth/src/handler.rs
+++ cita-auth/src/handler.rs
@@ -61,7 +61,7 @@ pub fn verify_tx_group_service(
             let tx_hash = H256::from_slice(req.get_tx_hash());
             let response = { verifier.read().verify_tx(&req) };
             {
-                if response.get_ret()!=Ret::NotReady {
+                if response.get_ret() != Ret::NotReady {
                     cache.write().insert(tx_hash, response.clone());
                 }
             }
