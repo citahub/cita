3b4f52c0fbfe8a0c9b9c2d77307936cfe0e14886
Qian Linfeng <thewawar@gmail.com>
2018-06-06 11:39:36 +0800

    Can run docker with root

--- env.sh
+++ env.sh
@@ -25,6 +25,12 @@ SOURCE_DIR=`pwd`
 CONTAINER_NAME="cita_build${SOURCE_DIR//\//_}"
 CARGO_HOME=/opt/.cargo
 WORKDIR=/opt/cita
+USER_ID=`id -u $USER`
+USER_NAME="user"
+
+if [ "${USER_ID}" = "0" ]; then
+    USER_NAME="root"
+fi
 
 mkdir -p ${HOME}/.docker_cargo/git
 mkdir -p ${HOME}/.docker_cargo/registry
@@ -40,18 +46,17 @@ else
            --volume ${HOME}/.docker_cargo/registry:${CARGO_HOME}/registry \
            --volume ${HOME}/.docker_cargo/git:${CARGO_HOME}/git \
            --volume ${LOCALTIME_PATH}:/etc/localtime \
-           --env USER_ID=`id -u $USER` \
+           --env USER_ID=${USER_ID} \
            --workdir ${WORKDIR} \
            --name ${CONTAINER_NAME} ${DOCKER_IMAGE} \
            /bin/bash -c "while true;do sleep 100;done"
-    sleep 20
 fi
 
 test -t 1 && USE_TTY="-t"
 
 if [ $# -gt 0 ]; then
-    docker exec -i ${USE_TTY} ${CONTAINER_NAME} /usr/bin/gosu user "$@"
+    docker exec -i ${USE_TTY} ${CONTAINER_NAME} /usr/bin/gosu ${USER_NAME} "$@"
 else
     docker exec -i ${USE_TTY} ${CONTAINER_NAME} \
-        /bin/bash -c "stty cols $(tput cols) rows $(tput lines) && bash"
+        /bin/bash -c "stty cols $(tput cols) rows $(tput lines) && /usr/bin/gosu ${USER_NAME} /bin/bash"
 fi
--- env_cn.sh
+++ env_cn.sh
@@ -25,6 +25,12 @@ SOURCE_DIR=`pwd`
 CONTAINER_NAME="cita_build${SOURCE_DIR//\//_}"
 CARGO_HOME=/opt/.cargo
 WORKDIR=/opt/cita
+USER_ID=`id -u $USER`
+USER_NAME="user"
+
+if [ "${USER_ID}" = "0" ]; then
+    USER_NAME="root"
+fi
 
 mkdir -p ${HOME}/.docker_cargo/git
 mkdir -p ${HOME}/.docker_cargo/registry
@@ -40,18 +46,17 @@ else
            --volume ${HOME}/.docker_cargo/registry:${CARGO_HOME}/registry \
            --volume ${HOME}/.docker_cargo/git:${CARGO_HOME}/git \
            --volume ${LOCALTIME_PATH}:/etc/localtime \
-           --env USER_ID=`id -u $USER` \
+           --env USER_ID=${USER_ID} \
            --workdir ${WORKDIR} \
            --name ${CONTAINER_NAME} ${DOCKER_IMAGE} \
            /bin/bash -c "echo -e '[source.crates-io]\nregistry = \"https://github.com/rust-lang/crates.io-index\"\nreplace-with = \"ustc\"\n[source.ustc]\nregistry = \"https://mirrors.ustc.edu.cn/crates.io-index\"' | sudo tee /opt/.cargo/config;while true;do sleep 100;done"
-    sleep 20
 fi
 
 test -t 1 && USE_TTY="-t"
 
 if [ $# -gt 0 ]; then
-    docker exec -i ${USE_TTY} ${CONTAINER_NAME} /usr/bin/gosu user "$@"
+    docker exec -i ${USE_TTY} ${CONTAINER_NAME} /usr/bin/gosu ${USER_NAME} "$@"
 else
     docker exec -i ${USE_TTY} ${CONTAINER_NAME} \
-        /bin/bash -c "stty cols $(tput cols) rows $(tput lines) && bash"
+        /bin/bash -c "stty cols $(tput cols) rows $(tput lines) && /usr/bin/gosu ${USER_NAME} /bin/bash"
 fi
