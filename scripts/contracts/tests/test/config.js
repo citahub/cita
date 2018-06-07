09f5f447356d80f336411ae93e149b969258de5e
kaikaifeng <kaikai@cryptape.com>
2018-06-07 20:06:07 +0800

    Fix test case of system contract. [skip ci]

--- scripts/contracts/tests/test/config.js
+++ scripts/contracts/tests/test/config.js
@@ -288,22 +288,25 @@ module.exports = {
       },
     },
     chain_manager: {
-      abi: [{
-        constant: true, inputs: [], name: 'getChainId', outputs: [{ name: '', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function',
+      abi:
+      [{
+        constant: true, inputs: [], name: 'getChainId', outputs: [{ name: '', type: 'uint32' }], payable: false, stateMutability: 'view', type: 'function',
       }, {
-        constant: false, inputs: [{ name: 'id', type: 'uint256' }], name: 'disableSideChain', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
+        constant: true, inputs: [{ name: 'id', type: 'uint32' }], name: 'getAuthorities', outputs: [{ name: '', type: 'address[]' }], payable: false, stateMutability: 'view', type: 'function',
       }, {
-        constant: true, inputs: [{ name: 'id', type: 'uint256' }], name: 'getAuthorities', outputs: [{ name: '', type: 'address[]' }], payable: false, stateMutability: 'view', type: 'function',
+        constant: false, inputs: [], name: 'getParentChainId', outputs: [{ name: '', type: 'uint32' }], payable: false, stateMutability: 'nonpayable', type: 'function',
       }, {
-        constant: true, inputs: [], name: 'getParentChainId', outputs: [{ name: '', type: 'uint256' }], payable: false, stateMutability: 'view', type: 'function',
+        constant: false, inputs: [{ name: 'id', type: 'uint32' }], name: 'enableSideChain', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
       }, {
-        constant: false, inputs: [{ name: 'addrs', type: 'address[]' }], name: 'newSideChain', outputs: [{ name: '', type: 'uint256' }], payable: false, stateMutability: 'nonpayable', type: 'function',
+        constant: false, inputs: [{ name: 'sideChainId', type: 'uint32' }, { name: 'addrs', type: 'address[]' }], name: 'newSideChain', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
       }, {
-        constant: false, inputs: [{ name: 'id', type: 'uint256' }], name: 'enableSideChain', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
+        constant: false, inputs: [{ name: 'id', type: 'uint32' }], name: 'disableSideChain', outputs: [], payable: false, stateMutability: 'nonpayable', type: 'function',
       }, {
-        constant: true, inputs: [{ name: '', type: 'uint256' }], name: 'sideChains', outputs: [{ name: 'status', type: 'uint8' }], payable: false, stateMutability: 'view', type: 'function',
+        constant: true, inputs: [{ name: '', type: 'uint32' }], name: 'sideChains', outputs: [{ name: 'status', type: 'uint8' }], payable: false, stateMutability: 'view', type: 'function',
       }, {
-        inputs: [{ name: '_id', type: 'uint256' }, { name: '_pid', type: 'uint256' }, { name: '_addrs', type: 'address[]' }], payable: false, stateMutability: 'nonpayable', type: 'constructor',
+        inputs: [{ name: '_pid', type: 'uint32' }, { name: '_addrs', type: 'address[]' }], payable: false, stateMutability: 'nonpayable', type: 'constructor',
+      }, {
+        anonymous: false, inputs: [{ indexed: true, name: 'errorType', type: 'uint8' }, { indexed: false, name: 'msg', type: 'string' }], name: 'ErrorLog', type: 'event',
       }],
       addr: '0x00000000000000000000000000000000000000ce',
     },
