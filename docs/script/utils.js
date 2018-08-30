64daf68dc668c58242a8824daa2be25ef216b21d
Keith <keithwhisper@gmail.com>
2018-08-30 10:37:47 +0800

    update: redirect v0.17 to Latest and hide v0.18 now

--- docs/script/utils.js
+++ docs/script/utils.js
@@ -1,2 +1,8 @@
 var log = console.log.bind(console);
-var baseUrlGen = (ver, lng) => `https://raw.githubusercontent.com/cryptape/cita/${ver|| 'develop'}/docs/${lng || 'zh-CN'}/Latest`
+var dir = (ver) => {
+  if (ver === 'v0.17') {
+    return 'Latest'
+  }
+  return ''
+}
+var baseUrlGen = (ver, lng) => `https://raw.githubusercontent.com/cryptape/cita/${ver|| 'develop'}/docs/${lng || 'zh-CN'}/${dir(ver)}`
