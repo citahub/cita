128b2abb8fa17204828c3f5d3b5454942145172a
Keith <keithwhisper@gmail.com>
2018-09-21 15:24:28 +0800

    update: fetch doc tags from github[skip ci]

--- docs/script/utils.js
+++ docs/script/utils.js
@@ -1,8 +1,30 @@
 var log = console.log.bind(console);
 var dir = (ver) => {
-  if (ver === 'v0.17'||ver==='v0.18') {
+  if (['v0.17', 'v0.18rc', 'v0.18'].indexOf(ver) > -1) {
     return 'Latest'
   }
   return ''
 }
 var baseUrlGen = (ver, lng) => `https://raw.githubusercontent.com/cryptape/cita/${ver|| 'develop'}/docs/${lng || 'zh-CN'}/${dir(ver)}`
+
+const loadTags = () => {
+  // let tag
+  const tagList = document.querySelector("#tag-list")
+  const fragment = document.createDocumentFragment()
+  const appendTags = (tagNames) => {
+    tagNames.forEach(tagName => {
+      const item = document.createElement('li')
+      item.innerHTML = `<a href="javascript:toVersion('${tagName}')">${tagName}</a>`
+      fragment.appendChild(item)
+    })
+    tagList.appendChild(fragment)
+
+  }
+  if (fetch) {
+    fetch('https://api.github.com/repos/cryptape/cita/tags').then(res => res.json()).then(tags => tags.slice(0, -5).map(tag => tag.name)).then(tagNames => {
+      appendTags(tagNames)
+    })
+  } else {
+    appendTags(['v0.17', 'v0.18rc', 'v0.18'])
+  }
+}
