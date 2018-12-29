64cf6a5da67a256bbd7fb2f78e5307c511684d89
zeroqn <zeroqn@cryptape.com>
2018-12-29 10:32:25 +0800

    chore: box_executor: remove ususe `chain/core` dependency

--- tests/box_executor/src/runner.rs
+++ tests/box_executor/src/runner.rs
@@ -2,7 +2,6 @@ extern crate bincode;
 extern crate cita_crypto as crypto;
 extern crate cita_types;
 extern crate clap;
-extern crate core;
 extern crate dotenv;
 extern crate logger;
 extern crate proof;
