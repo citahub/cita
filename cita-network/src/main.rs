52bb59c6b73a56a9ebee3d0852b819d8f44e0569
leeyr <leeyr@cryptape.com>
2019-02-18 15:34:11 +0800

    add lisence header and some comments

--- cita-network/src/main.rs
+++ cita-network/src/main.rs
@@ -1,3 +1,83 @@
+// CITA
+// Copyright 2016-2019 Cryptape Technologies LLC.
+
+// This program is free software: you can redistribute it
+// and/or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any
+// later version.
+
+// This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//! ## Summary
+//!
+//! One of the CITA's core components is used to implement the peer-to-peer network
+//! and provide point-to-point connection interaction.
+//!
+//! ### Message queuing situation
+//!
+//! 1. Subscribe channel
+//!
+//!     |       Queue       | PubModule | Message Type          |
+//!     | ----------------- | --------- | --------------------- |
+//!     | network_tx        | Auth      | Request               |
+//!     | network_consensus | Consensus | CompactSignedProposal |
+//!     | network_consensus | Consensus | RawBytes              |
+//!     | network           | Chain     | Status                |
+//!     | network           | Chain     | SyncResponse          |
+//!     | network           | Jonsonrpc | RequestNet            |
+//!     | network           | Auth      | GetBlockTxn           |
+//!     | network           | Auth      | BlockTxn              |
+//!
+//! 2. Publish channel
+//!
+//!     |       Queue       | PubModule | SubModule           | Message Type          |
+//!     | ----------------- | --------- | ------------------- | --------------------- |
+//!     | network           | Net       | Chain, Executor     | SyncResponse          |
+//!     | network           | Net       | Snapshot            | SnapshotResp          |
+//!     | network           | Net       | Jsonrpc             | Response              |
+//!     | network_tx        | Net       | Auth                | Request               |
+//!     | network_consensus | Net       | Consensus           | ComapctSignedProposal |
+//!     | network_consensus | Net       | Consensus           | RawBytes              |
+//!     | network           | Net       | Auth                | BlockTxn              |
+//!     | network           | Net       | Auth                | GetBlockTxn           |
+//!
+//! ### p2p binary protocol
+//! | Start      | Full length | Key length | Key value      | Message value    |
+//! | ---------- | ----------- | ---------- | -------------- | ---------------- |
+//! | \xDEADBEEF | u32         | u8(byte)   | bytes of a str | a serialize data |
+//!
+//! full_len = 1 + key_len + body_len
+//!
+//! ### Key behavoir
+//!
+//! the key struct:
+//!
+//! - [`Connection`]
+//! - [`NetWork`]
+//! - [`Synchronizer`]
+//!
+//! In addition to the `tokio_server`, there is an `Arc<Connection>` for
+//! this structure in almost all the threads of this module to confirm that the node is alive,
+//! increase or decrease nodes, consensus message broadcasts, authentication message broadcasts,
+//! node status broadcasts, synchronization node blocks Height and so on.
+//!
+//! About binary protocol encoding and decoding, please look at module `citaprotocol`, the fuction
+//! [`pubsub_message_to_network_message`] and [`network_message_to_pubsub_message`].
+//!
+//! [`Connection`]: ./connection/struct.Connection.html
+//! [`NetWork`]: ./network/struct.NetWork.html
+//! [`Synchronizer`]: ./synchronizer/struct.Synchronizer.html
+//! [`pubsub_message_to_network_message`]: ./citaprotocol/fn.pubsub_message_to_network_message.html
+//! [`network_message_to_pubsub_message`]: ./citaprotocol/fn.network_message_to_pubsub_message.html
+//!
+
 pub mod citaprotocol;
 pub mod config;
 pub mod mq_client;
--- cita-network/src/synchronizer.rs
+++ cita-network/src/synchronizer.rs
@@ -1,5 +1,5 @@
 // CITA
-// Copyright 2016-2018 Cryptape Technologies LLC.
+// Copyright 2016-2019 Cryptape Technologies LLC.
 
 // This program is free software: you can redistribute it
 // and/or modify it under the terms of the GNU General Public
@@ -14,6 +14,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::mq_client::{MqClient, PubMessage};
 use crate::node_manager::{BroadcastReq, NodesManagerClient, SingleTxReq};
 use crossbeam_channel;
