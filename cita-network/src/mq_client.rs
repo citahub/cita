d1fd1020688179ebdfdc3c1ec9baf6cd1c3336d8
leeyr <leeyr@cryptape.com>
2019-02-26 14:03:29 +0800

    cargo update: fix some interface changed

--- cita-network/src/mq_client.rs
+++ cita-network/src/mq_client.rs
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::sync::mpsc::Sender;
+use crossbeam_channel::Sender;
 
 #[derive(Clone)]
 pub struct MqClient {
