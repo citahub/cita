76e71f19250964864234edd1d4d6cd95a94ffb5f
kaikai1024 <kaikaifeng1024@gmail.com>
2019-05-13 11:33:05 +0800

    Check the shell and fix.

--- scripts/cita_config.sh
+++ scripts/cita_config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Enviroments
-CITA_BIN=$(realpath $(dirnamme $0))
-CITA_SCRIPTS=$(dirname $CITA_BIN)/scripts
+CITA_BIN=$(realpath "$(dirnamme "$0")")
+CITA_SCRIPTS=$(dirname "$CITA_BIN")/scripts
 
 # Wrap the create script.
-if [ -e $CITA_SCRIPTS/create_cita_config.py ]; then
-    $CITA_SCRIPTS/create_cita_config.py $@
+if [ -e "$CITA_SCRIPTS"/create_cita_config.py ]; then
+    "$CITA_SCRIPTS"/create_cita_config.py "$@"
 else
     echo -e "\033[0;31mPlease run this command after build 🎨"
 fi
