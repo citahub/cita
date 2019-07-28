c858f643782ad9f946628ed8ba0a8b6b10902799
yubo <yubo@cryptape.com>
2019-07-28 05:55:52 +0000

    [skip ci]add func

--- cita-executor/core/src/trie_db.rs
+++ cita-executor/core/src/trie_db.rs
@@ -53,8 +53,9 @@ where
         }
     }
 
-    fn remove(&self, _key: &[u8]) -> Result<(), Self::Error> {
-        unimplemented!()
+    fn remove(&self, key: &[u8]) -> Result<(), Self::Error> {
+        self.cache.write().remove(key);
+        self.db.remove(Some(DataCategory::State), key)
     }
 
     fn insert_batch(&self, keys: Vec<Vec<u8>>, values: Vec<Vec<u8>>) -> Result<(), Self::Error> {
@@ -67,8 +68,14 @@ where
         Ok(())
     }
 
-    fn remove_batch(&self, _keys: &[Vec<u8>]) -> Result<(), Self::Error> {
-        unimplemented!()
+    fn remove_batch(&self, keys: &[Vec<u8>]) -> Result<(), Self::Error> {
+        {
+            let mut cache = self.cache.write();
+            for key in keys {
+                cache.remove(key);
+            }
+        }
+        self.db.remove_batch(Some(DataCategory::State), keys)
     }
 
     fn flush(&self) -> Result<(), Self::Error> {
