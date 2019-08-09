5c1008ac75ff892342cea0a72d1e4932c550ca61
ouwenkg <2630582710@qq.com>
2019-08-09 10:38:51 +0800

    [error] remove useless error types

--- cita-chain/types/src/errors/error.rs
+++ cita-chain/types/src/errors/error.rs
@@ -16,53 +16,20 @@
 
 //! General error types for use in ethcore.
 
-use cita_ed25519::Error as EthkeyError;
-
-use cita_trie::TrieError;
 use std::fmt;
 
 use super::execution::ExecutionError;
-use super::util::UtilError;
 
-#[allow(unknown_lints, clippy::large_enum_variant)] // TODO clippy
 #[derive(Debug)]
-/// General error type which should be capable of representing all errors in ethcore.
 pub enum Error {
-    /// Error concerning a utility.
-    Util(UtilError),
-    /// Unknown engine given.
-    UnknownEngineName(String),
     /// Error concerning EVM code execution.
     Execution(ExecutionError),
-    /// PoW hash is invalid or out of date.
-    PowHashInvalid,
-    /// The value of the nonce or mishash is invalid.
-    PowInvalid,
-    /// Error concerning TrieDBs
-    Trie(TrieError),
-    /// Standard io error.
-    StdIo(::std::io::Error),
-    /// Snappy error.
-    Snappy(snappy::SnappyError),
-    /// Ethkey error.
-    Ethkey(EthkeyError),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Error::Util(ref err) => err.fmt(f),
-            // Error::Block(ref err) => err.fmt(f),
             Error::Execution(ref err) => err.fmt(f),
-            Error::UnknownEngineName(ref name) => {
-                f.write_fmt(format_args!("Unknown engine name ({})", name))
-            }
-            Error::PowHashInvalid => f.write_str("Invalid or out of date PoW hash."),
-            Error::PowInvalid => f.write_str("Invalid nonce or mishash"),
-            Error::Trie(ref err) => err.fmt(f),
-            Error::StdIo(ref err) => err.fmt(f),
-            Error::Snappy(ref err) => err.fmt(f),
-            Error::Ethkey(ref err) => err.fmt(f),
         }
     }
 }
@@ -70,41 +37,5 @@ impl fmt::Display for Error {
 impl From<ExecutionError> for Error {
     fn from(err: ExecutionError) -> Error {
         Error::Execution(err)
-    }
-}
-
-impl From<::rlp::DecoderError> for Error {
-    fn from(err: ::rlp::DecoderError) -> Error {
-        Error::Util(UtilError::Decoder(err))
-    }
-}
-
-impl From<UtilError> for Error {
-    fn from(err: UtilError) -> Error {
-        Error::Util(err)
-    }
-}
-
-impl From<TrieError> for Error {
-    fn from(err: TrieError) -> Error {
-        Error::Trie(err)
-    }
-}
-
-impl From<::std::io::Error> for Error {
-    fn from(err: ::std::io::Error) -> Error {
-        Error::StdIo(err)
-    }
-}
-
-impl From<snappy::SnappyError> for Error {
-    fn from(err: snappy::SnappyError) -> Error {
-        Error::Snappy(err)
-    }
-}
-
-impl From<EthkeyError> for Error {
-    fn from(err: EthkeyError) -> Error {
-        Error::Ethkey(err)
     }
 }
--- cita-chain/types/src/errors/mod.rs
+++ cita-chain/types/src/errors/mod.rs
@@ -1,7 +1,5 @@
 mod error;
 mod execution;
-mod util;
 
-pub use self::util::UtilError;
 pub use error::Error;
 pub use execution::ExecutionError;
