da876e0bb49ac7305b7c14a7367571acacd9c215
leeyr <leeyr@cryptape.com>
2019-08-27 15:32:34 +0800

    Change GPL to apache 2.0

--- tests/json-test/src/state_test.rs
+++ tests/json-test/src/state_test.rs
@@ -1,3 +1,17 @@
+// Copyright Cryptape Technologies LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
 use crate::helper::{secret_2_address, string_2_bytes, string_2_h256, string_2_u256};
 use crate::json::state::Test;
 
@@ -13,6 +27,7 @@ use cita_types::U256;
 use cita_vm::state::{State, StateObjectInfo};
 use core_executor::tx_gas_schedule::TxGasSchedule;
 use libproto::blockchain::Transaction as ProtoTransaction;
+
 use std::fs;
 use std::sync::Arc;
 
