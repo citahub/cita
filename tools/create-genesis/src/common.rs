5db9bd8cfdeb8e6135046fc76fe2120282090eab
Yaorong <38514341+leeyr338@users.noreply.github.com>
2019-08-30 18:50:22 +0800

    Change GPL to apache 2.0 (#827)

--- tools/create-genesis/src/common.rs
+++ tools/create-genesis/src/common.rs
@@ -1,3 +1,17 @@
+// Copyright Cryptape Technologies LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 pub fn string_2_bytes(value: String) -> Vec<u8> {
     let v = Box::leak(value.into_boxed_str());
     let v = cita_types::clean_0x(v);
