703332164397dbae01d2053ac3fff5ee593df62c
leeyr <leeyr@cryptape.com>
2019-09-10 14:27:59 +0800

    Hard code release version

--- cita-auth/build.rs
+++ cita-auth/build.rs
@@ -18,7 +18,9 @@ use std::env;
 
 use util::build_info::gen_build_info;
 
+const VERSION: &str = "1.0.0";
+
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    gen_build_info(out_dir.as_ref(), "build_info.rs");
+    gen_build_info(out_dir.as_ref(), "build_info.rs", VERSION.to_owned());
 }
